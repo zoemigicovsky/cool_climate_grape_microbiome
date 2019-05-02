1da4a8c4652f1cbb1ee342b99c38115961e39c90
zoemigicovsky <zoe.migicovsky@dal.ca>
2019-05-02 11:21:14 -0300

    update permanova with fungal analysis

--- permanova_analysis.R
+++ permanova_analysis.R
@@ -4,6 +4,10 @@ bacteria_meta <- read.delim("bacteria/root_depth_bacteria_metadata_grape.tsv", r
 
 bacteria_bray <- read.delim("bacteria/diversity_grape/bray_curtis_distance_matrix_exported/distance-matrix.tsv", header = T, row.names=1)
 
+#Reduce meta data down to samples in the distance matrix
+bacteria_meta <- bacteria_meta[rownames(bacteria_bray),]
+
+
 permanova_bacteria <- adonis(bacteria_bray ~ rootstock+root_depth,
        data = bacteria_meta, permutations=999, method = "bray")
 
@@ -31,11 +35,28 @@ fungi_meta <- read.delim("fungi/root_depth_fungi_metadata_grape.tsv", row.names=
 
 fungi_bray <- read.delim("fungi/diversity_grape/bray_curtis_distance_matrix_exported/distance-matrix.tsv", header = T, row.names=1)
 
+#Reduce meta data down to samples in the distance matrix
+fungi_meta <- fungi_meta[rownames(fungi_bray),]
+
 permanova_fungi <- adonis(fungi_bray ~ rootstock+root_depth,
                              data = fungi_meta, permutations=999, method = "bray")
 
-#Error in G * t(hat) : non-conformable arrays
- 
-#I can't seem to run the analysis using the fungal data 
+permanova_fungi
 
-#If I can get the fungal part working we can put these results into a (tiny) table in the paper or just report them in the text. 
+#Call:
+# adonis(formula = fungi_bray ~ rootstock + root_depth, data = fungi_meta,      permutations = 999, method = "bray") 
+# 
+# Permutation: free
+# Number of permutations: 999
+# 
+# Terms added sequentially (first to last)
+# 
+#             Df SumsOfSqs MeanSqs F.Model      R2 Pr(>F)  
+# rootstock   1   0.13270 0.13270 0.96585 0.08081  0.369  
+# root_depth  2   0.54766 0.27383 1.99308 0.33351  0.020 *
+#   Residuals   7   0.96174 0.13739         0.58568         
+# Total      10   1.64210                 1.00000         
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#Root depth is a significant fator (R2=0.33351, p=0.020) but rootstock is not for fungal microbiome. 
